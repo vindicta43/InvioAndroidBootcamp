1ebf7aece6945419ec3c4cd3781f3656d7240883
Alperen Erdoğan <alperen.erdogan.neu@gmail.com>
2022-09-30 14:12:19 +0300

    initial commit and Odev1

---
+++ Odev1/src/Odev1.kt
@@ -0,0 +1,36 @@
+/**
+ * Created by Alperen on 26.09.2022.
+ */
+
+// Alperen Erdoğan
+fun main() {
+    // Islem degiskenleri
+    val islemNo: Int = 123
+    val ogrNo: Int = 456
+    val kitapNo: Int = 15
+    val aTarih: String = "27/08/2022"
+    val vTarih: String = "27/09/2022"
+
+    // Ogrenci degiskenleri
+    val ogrAd: String = "Ahmet"
+    val ogrSoyad: String = "Kurt"
+    val cinsiyet: String = "Erkek"
+    val dTarih: String = "05/05/1995"
+    val sinif: Int = 11
+
+    // Kitap degiskenleri
+    val isbnNo: Int = 112233
+    val kitapAdi: String = "Suç ve Ceza"
+    val yazarAdi: String = "Dostoevsky"
+    val turNo: Int = 5
+    val sayfaSayisi: Int = 540
+    val puan: Double = 4.8
+
+    // Yazar degiskenleri
+    val yazarNo: Int = 12345
+    val yazarAd: String = "Fyodor Mihayloviç"
+    val yazarSoyad: String = "Dostoevsky"
+
+    // Tur degiskenleri
+    val turAdi: String = "Roman"
+}
