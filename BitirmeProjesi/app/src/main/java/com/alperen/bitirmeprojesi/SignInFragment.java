67d129490e3f31eb1743e05ed4f86c221eb93306
Alperen Erdoğan <alperen.erdogan.neu@gmail.com>
2022-10-31 18:06:49 +0300

    sign in page

--- BitirmeProjesi/app/src/main/java/com/alperen/bitirmeprojesi/SignInFragment.java
+++ BitirmeProjesi/app/src/main/java/com/alperen/bitirmeprojesi/SignInFragment.java
@@ -8,9 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class BlankFragment extends Fragment {
+public class SignInFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
 }
